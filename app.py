47cd68f2af92cff7664203b2e7b265e01546b3c7
Jamie <bartonjamie109@gmail.com>
2024-04-15 16:05:22 -0700

    General encode fucntion is wokring, need to come up with a way to avoid list out of bounds errors for cases where the offset wraps around

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 import os
 import sys
+import string
 
 def encode(uFile, offset):
+
+    f = open(uFile, "r")
+    encF = open("encryptedFile.txt", "w")
+    alph = list(string.ascii_lowercase)
+
+    for line in f:
+        for letter in line:
+            letter.lower()
+            if letter in alph:
+                letterPos = alph.index(letter)
+                letterPos +=  offset
+                encLetter = alph[letterPos]
+                encF.write(encLetter)
+    
+    f.close()
+    encF.close()
+
+
+            
+    
     return
 
 
@@ -12,24 +33,33 @@ def checkFile(uFile):
     print(uFile)
     
     if os.path.isfile(uFile):
-        print("looks good")
+        print("looks good\n")
         True
     else:
         print("Please enter a valid file name, thank you.")
         sys.exit()
 
 def main():
+    print(string.ascii_letters)
     uInput = input("Would you like to encode or decode today?   ")
 
     if uInput.lower() == "encode":
         uFile = input("Please enter the file name you would like to encode:  ")
         checkFile(uFile)
-        offset = input("Please enter the offset of the file to be encoded:  ")
-        encode(uFile, offset)
+        offset = int(input("Please enter the offset of the file to be encoded:  "))
+        if type(offset) == int:
+            encode(uFile, offset)
+        else:
+            print("Offset must be an integer\n")
+
+
+
     elif uInput.lower() == "decode":
         uFile = input("Please enter the file name you would like to deocde:  ")
         checkFile(uFile)
         decode(uFile, offset)
+
+
     else:
         print("Please enter encode or decode, thank you!")    
 
